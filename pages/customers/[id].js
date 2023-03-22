e26668018a8d6ac22cc221de66f84c0b65dc8b94
aly8i <ABY00003@students.aust.edu.lb>
2023-03-22 04:22:04 +0200

    v2

--- pages/customers/[id].js
+++ pages/customers/[id].js
@@ -80,7 +80,7 @@ const EditUser = ({user}) => {
     <div className={styles.new}>
       <div className={styles.newContainer}>
         <div className={styles.top}>
-          <h1>Add a Customer</h1>
+          <h1>Edit your Customer</h1>
         </div>
         <div className={styles.bottom}>
           <div className={styles.right}>
